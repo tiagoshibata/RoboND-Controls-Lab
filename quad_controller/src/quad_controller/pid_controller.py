c6b6b963440bf6bffa84e7b8045bbe31f3119ab8
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-10-16 13:37:14 -0700

    Add quadcopter PID control

--- quad_controller/src/quad_controller/pid_controller.py
+++ quad_controller/src/quad_controller/pid_controller.py
@@ -5,35 +5,59 @@
 
 
 class PIDController:
-    def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 10):
-        #TODO
-        pass
+    def __init__(self, kp = 0., ki = 0., kd = 0., max_windup = 10):
+        self.reset()
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.max_windup = max_windup
+
+
     def reset(self):
-        #TODO
-        pass
+        self.kp = 0.
+        self.ki = 0.
+        self.kd = 0.
+        self.max_windup = 0.
+        self.last_diff = self.last_time = self.set_point = self.error_integral = self.last_error = 0.
+
 
     def setTarget(self, target):
-        #TODO
-        pass
+        self.set_point = target
+
 
     def setKP(self, kp):
-        #TODO
-        pass
+        self.kp = kp
+
 
     def setKI(self, ki):
-        #TODO
-        pass
+        self.ki = ki
+
 
     def setKD(self, kd):
-        #TODO
-        pass
+        self.kd = kd
+
 
     def setMaxWindup(self, max_windup):
-        #TODO
-        pass
+        self.max_windup = max_windup
+
 
     def update(self, measured_value, timestamp):
-        #TODO
-        pass
+        error = self.set_point - measured_value
+        delta = timestamp - self.last_time
 
+        self.error_integral += error * delta
+        self.error_integral = min(max(self.error_integral, -self.max_windup), self.max_windup)
 
+        p = self.kp * error
+        i = self.ki * self.error_integral
+
+        diff = error - self.last_error
+        alpha = .8
+        filtered_diff = alpha * diff / delta + (1 - alpha) * self.last_diff
+        d = self.kd * filtered_diff
+
+        self.last_error = error
+        self.last_time = timestamp
+        self.last_diff = filtered_diff
+
+        return p + i + d
